327fd1ebf863784ed0c7e1dc4a1c87a15593d728
Richard Korthuis <richardkorthuis@acato.nl>
2025-03-06 10:46:45 +0100

    Fix P2P connections

--- src/rest_api/class-openpub-controller.php
+++ src/rest_api/class-openpub-controller.php
@@ -148,9 +148,19 @@ class Openpub_Controller extends \WP_REST_Posts_Controller {
 			$taxonomies = [];
 		}
 
+		$connections_config_file = $openpub_plugin_dir_path . 'config/p2p_connections.php';
+
+		// Check if file exists.
+		if ( file_exists( $connections_config_file ) ) {
+			$connections = require $connections_config_file;
+		} else {
+			$connections = [];
+		}
+
 		// Set the taxonomies for the OpenPub plugin.
 		$open_pub_plugin = new Plugin( 'openpub' );
 		$open_pub_plugin->config->set( 'taxonomies', $taxonomies );
+		$open_pub_plugin->config->set( 'p2p_connections', $connections );
 		self::$open_pub_plugin = $open_pub_plugin;
 
 		// Retrieve the list of registered collection query parameters.
@@ -164,6 +174,20 @@ class Openpub_Controller extends \WP_REST_Posts_Controller {
 
 		// Set the post type to openpub-item and prepare the query.
 		$args['post_type'] = 'openpub-item';
+
+		// Check if OpenPub item has geometry data and geometry is not empty.
+		// phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_query -- We need to check for the existence of the geometry field.
+		$args['meta_query'] = [
+			[
+				'key'     => 'geometry',
+				'compare' => 'EXISTS',
+			],
+			[
+				'key'     => 'geometry',
+				'compare' => '!=',
+				'value'   => '',
+			],
+		];
 
 		$query_args = $this->prepare_items_query( $args, $request );
 
